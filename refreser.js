201cff498c501842a64962a16b7dd7eb8209ae1b
ajalal24 <ajalal24@penguinhall.org>
2022-03-27 16:48:10 -0400

    Changes

---
+++ refreser.js
@@ -0,0 +1,25 @@
+// console.log('Hello world!');
+
+var message = 'How are you today?';
+// console.log(message)
+
+var greeting = 'Hello'
+var name = 'Sue'
+// console.log(greeting + ' ' + name + '!')
+
+var age = '13'
+// console.log('You are ' + age)
+
+// console.log('In ten years you will be ' + age + 10)
+
+// Part 2
+
+if ( name == 'Bob') {
+    console.log('I knew it was you, Bob!')
+} else {
+    console.log('Hey Sue, you\'re not Bob!')
+}
+
+var iAmWearingABelt = true;
+var iAmWearingGlasses = false;
+
