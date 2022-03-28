6f45976af15bcae8f25d0c1bb38e8a2ab1bfe7ad
ajalal24 <ajalal24@penguinhall.org>
2022-03-28 13:19:58 -0400

    Drama Fest

--- refreser.js
+++ refreser.js
@@ -1,25 +1,32 @@
 // console.log('Hello world!');
 
-var message = 'How are you today?';
-// console.log(message)
+let message = 'How are you today?';
+// console.log(message);
 
-var greeting = 'Hello'
-var name = 'Sue'
-// console.log(greeting + ' ' + name + '!')
+let greeting = 'Hello';
+let name = 'Sue';
+// console.log(greeting + ' ' + name + '!');
 
-var age = '13'
-// console.log('You are ' + age)
+let age = 13;
+// console.log('You are ' + age);
 
-// console.log('In ten years you will be ' + age + 10)
+// console.log('In ten years you will be ' + (age + 10));
 
 // Part 2
 
-if ( name == 'Bob') {
-    console.log('I knew it was you, Bob!')
+if ( name === 'Bob') {
+    console.log('I knew it was you, ' + name +'!')
 } else {
-    console.log('Hey Sue, you\'re not Bob!')
+    console.log('Hey ' + name + ', you\'re not Bob!')
 }
 
-var iAmWearingABelt = true;
-var iAmWearingGlasses = false;
+let iAmWearingABelt = true;
+let iAmWearingGlasses = false;
 
+if (iAmWearingABelt && iAmWearingGlasses) {
+    console.log('Probably not Mr. Gorton.')
+} else if (iAmWearingABelt && !iAmWearingGlasses) {
+    console.log('Probably is Mr. Gorton.')
+} else {
+    console.log('Just some dude.')
+}
