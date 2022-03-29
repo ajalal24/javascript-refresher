2f253a40896c1473b07793b4fb24417d7c440245
ajalal24 <ajalal24@penguinhall.org>
2022-03-29 10:57:25 -0400

    More changes

--- refreser.js
+++ refreser.js
@@ -14,19 +14,35 @@ let age = 13;
 
 // Part 2
 
-if ( name === 'Bob') {
-    console.log('I knew it was you, ' + name +'!')
-} else {
-    console.log('Hey ' + name + ', you\'re not Bob!')
+// if ( name === 'Bob') {
+//     console.log('I knew it was you, ' + name +'!')
+// } else {
+//     console.log('Hey ' + name + ', you\'re not Bob!')
+// }
+
+// let iAmWearingABelt = false;
+// let iAmWearingGlasses = false;
+
+// if (iAmWearingABelt && iAmWearingGlasses) {
+//     console.log('Probably not Mr. Gorton.')
+// } else if (iAmWearingABelt && !iAmWearingGlasses) {
+//     console.log('Probably is Mr. Gorton.')
+// } else {
+//     console.log('Probably just some dude.')
+// }
+
+//Part 3
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i)
 }
 
-let iAmWearingABelt = true;
-let iAmWearingGlasses = false;
+tellMeMyAge();
 
-if (iAmWearingABelt && iAmWearingGlasses) {
-    console.log('Probably not Mr. Gorton.')
-} else if (iAmWearingABelt && !iAmWearingGlasses) {
-    console.log('Probably is Mr. Gorton.')
-} else {
-    console.log('Just some dude.')
+function tellMeMyAge() {
+    console.log('You are ' + age + ' years old!')
+} 
+
+function makePizza(type, size) {
+    console.log('One ' + size + ' ' + type + ' pizza comign right up!' )
 }
