7b547fe833c3e5716a6f936b324d756b00cc4f51
ajalal24 <ajalal24@penguinhall.org>
2022-04-01 10:21:07 -0400

    bopTalk

--- boptalk/boptalk.js
+++ boptalk/boptalk.js
@@ -1,4 +1,4 @@
-let text = 'sue';
+let text = 'Bob';
 
 for (let i = 0; i < text.length; i++) {
     if (text.charAt(i) == 'a' || text.charAt(i) == 'e' || text.charAt(i) == 'i' || text.charAt(i) == 'o' || text.charAt(i) == 'u') {
