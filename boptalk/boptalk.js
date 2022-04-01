360769de3ce5aa7b417ec81b20ccae74fa063594
ajalal24 <ajalal24@penguinhall.org>
2022-04-01 10:17:49 -0400

    BopTalk started

---
+++ boptalk/boptalk.js
@@ -0,0 +1,9 @@
+let text = 'bob';
+
+for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == 'a' || text.charAt(i) == 'e' || text.charAt(i) == 'i' || text.charAt(i) == 'o' || text.charAt(i) == 'u') {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
