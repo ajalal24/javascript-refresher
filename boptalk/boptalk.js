1879c98eb9fc74c71fbadbfb946904d430808d27
ajalal24 <ajalal24@penguinhall.org>
2022-04-06 19:56:20 -0400

    bopTalk changes

--- boptalk/boptalk.js
+++ boptalk/boptalk.js
@@ -1,11 +1,11 @@
 let inputMessage = 'Everything seems good!';
 
-let vowels = 'aeiouAEIOU';
+const vowels = 'aeiouAEIOU';
 
 let bopMessage = '';
 
 for (let i = 0; i < inputMessage.length; i++) {
-    letter = inputMessage[i];
+    let letter = inputMessage[i];
     if (vowels.includes(letter)) {
         bopMessage += letter + 'op';
     } else {
